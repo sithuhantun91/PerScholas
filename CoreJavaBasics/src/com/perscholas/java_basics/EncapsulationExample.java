e25fff8429cfce72fcb955f97c9d14f79539ef6a
sithuhantun91 <sithuhantun91@gmail.com>
2024-05-29 01:49:53 -0400

    Add Java file for GLAB - 303.9.1

---
+++ CoreJavaBasics/src/com/perscholas/java_basics/EncapsulationExample.java
@@ -0,0 +1,57 @@
+package com.perscholas.java_basics;
+
+public class EncapsulationExample {
+    public static void main(String[] args) {
+        HumanBeing h1 = new HumanBeing();
+        // using setters of HumanBeing
+        h1.setHeight(1.65f);
+        h1.setWeight(68);
+        h1.setBmi(calculateBmi(h1));
+        // using getters of HumanBeing
+        System.out.println("Person has " + h1.getWeight() + " kgs and is " + h1.getHeight()
+                + " meters in height, which results in BMI of " + h1.getBmi());
+    }
+
+    public static float calculateBmi(HumanBeing h1) {
+        return h1.getWeight() / (h1.getHeight() * h1.getHeight());
+    }
+}
+
+class HumanBeing {
+    private float weight;
+    private float height;
+    private float bmi;
+
+    public HumanBeing(float weight, float height, float bmi) {
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+    }
+
+    public HumanBeing() {
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(float bmi) {
+        this.bmi = bmi;
+    }
+}
