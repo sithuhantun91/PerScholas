d7b8d209e517bd6426aa185268db53bd71211c99
sithuhantun91 <sithuhantun91@gmail.com>
2024-06-03 17:00:12 -0400

    Delete hackerrank files

--- HackerRank44/src/com/perscholas/hackerrank44/Solution4.java
+++
@@ -1,8 +0,0 @@
-package com.perscholas.hackerrank44;
-
-public class Solution4 {
-    /*
-     * Write the regular expression in the blank space below
-     */
-    final static String regularExpression = "^[0]*1{1}[0]*$";
-}
