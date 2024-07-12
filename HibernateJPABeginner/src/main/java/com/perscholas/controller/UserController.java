f28e6fa57aa4b8eb6c9d20f9cf7a74fe738bd6c1
sithuhantun91 <sithuhantun91@gmail.com>
2024-07-12 14:03:07 -0400

    commit GLAB 305.4.2B

--- HibernateJPABeginner/src/main/java/com/perscholas/controller/UserController.java
+++ HibernateJPABeginner/src/main/java/com/perscholas/controller/UserController.java
@@ -1,21 +1,105 @@
 package com.perscholas.controller;
 
 import com.perscholas.model.User;
+import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class UserController {
     public static void main(String[] args) {
         SessionFactory factory = (new Configuration()).configure().buildSessionFactory();
         Session session = factory.openSession();
-        addUser(factory,session);
-        findUser(factory,session,3);
-        updateUser(session,3);
-        deleteUser(session,4);
+        // GLAB 305.4.1B
+        //addUser(factory,session);
+        //findUser(factory,session,3);
+        //updateUser(session,3);
+        //deleteUser(session,4);
+
+        // GLAB 305.4.2B
+        findUserHql(factory,session);
+        getRecordById(factory,session);
+        getRecords(session);
+        getMaxSalary(session);
+        getmaxSalaryGroupBy(session);
+        namedQueryExample(session);
+
         factory.close();
         session.close();
+    }
+
+    // GLAB 305.4.2B
+    public static void findUserHql(SessionFactory factory,Session session) {
+        String hqlFrom = "FROM User"; // Example of HQL to get all records of user
+        String hqlSelect = "SELECT u FROM User u";
+        //TypedQuery<User> query = session.createQuery(hqlFrom, User.class);
+        TypedQuery<User> query = session.createQuery(hqlSelect, User.class);
+        List<User> results = query.getResultList();
+        System.out.printf("%s%13s%17s%34s%n","|User Id","|Fullname","|Email","|Password");
+        for (User u:results) {
+            System.out.printf(" %-10d %-20s %-30s %s %n", u.getId(), u.getFullName(),
+                    u.getEmail(), u.getPassword());
+        }
+    }
+
+    // GLAB 305.4.2B
+    public static void getRecordById(SessionFactory factory, Session session) {
+        String hql = "FROM User u WHERE u.id > 2 ORDER BY u.salary DESC";
+        TypedQuery<User> query = session.createQuery(hql, User.class);
+        List<User> results = query.getResultList();
+        System.out.printf("%s%13s%17s%34s%21s%n", "|User Id", "|Full name", "|Email",
+                "|Password", "|Salary");
+        for (User u : results) {
+            System.out.printf(" %-10d %-20s %-30s %-23s %s %n", u.getId(),
+                    u.getFullName(), u.getEmail(), u.getPassword(), u.getSalary());
+        }
+    }
+
+    // GLAB 305.4.2B
+    public static void getRecords (Session session) {
+        TypedQuery<Object[]> query = session.createQuery(
+                "SELECT U.salary, U.fullName FROM User AS U", Object[].class);
+        List<Object[]> results = query.getResultList();
+        System.out.printf("%s%13s%n","Salary","City");
+        for (Object[] a : results) {
+            System.out.printf("%-16s%s%n",a[0],a[1]);
+        }
+    }
+
+    // GLAB 305.4.2B
+    public static void getMaxSalary(Session session) {
+        String hql = "SELECT max(U.salary) FROM User U";
+        TypedQuery<Object> query = session.createQuery(hql,Object.class);
+        Object result = query.getSingleResult();
+        System.out.printf("%s%s","Maximum Salary:",result);
+    }
+
+    // GLAB 305.4.2B
+    public static void getmaxSalaryGroupBy(Session session)
+    {
+        String hql = "SELECT SUM(U.salary), U.city FROM User U GROUP BY U.city";
+        TypedQuery query = session.createQuery(hql);
+        List<Object[]> result =query.getResultList();
+        for (Object[] o : result) {
+            System.out.println("Total salary " +o[0] +" | city: "+ o[1]);
+        }
+    }
+
+    // GLAB 305.4.2B
+    public static void namedQueryExample(Session session) {
+        String hql = "FROM User u WHERE u.id = :id";
+        TypedQuery<User> query = session.createQuery(hql, User.class);
+        query.setParameter("id", 2);
+        List<User> result = query.getResultList();
+        System.out.printf("%s%13s%17s%34s%21s%n", "|User Id", "|Full name", "|Email",
+                "|Password", "|Salary");
+        for (User u : result) {
+            System.out.printf(" %-10d %-20s %-30s %-23s %s %n", u.getId(),
+                    u.getFullName(), u.getEmail(), u.getPassword(), u.getSalary());
+        }
     }
 
     public static void addUser(SessionFactory factory, Session session) {
