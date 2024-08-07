83f7188f9795ef185f326d929c2dfdad9a5b0951
sithuhantun91 <sithuhantun91@gmail.com>
2024-08-07 16:01:55 -0400

    commit thymeleaf-tour2

---
+++ thymeleaf-tour2/src/main/java/com/example/thymeleaf_tour/ThymeleafTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTourApplication.class, args);
+	}
+
+}
