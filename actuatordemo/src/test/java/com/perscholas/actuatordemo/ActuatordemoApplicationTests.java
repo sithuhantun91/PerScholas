4ae4c67ee2baa71ec9b927c87011645b1eb36986
sithuhantun91 <sithuhantun91@gmail.com>
2024-08-18 00:18:43 -0400

    commit GLAB - 309.5.8 - SP10 - Spring Boot Actuator

---
+++ actuatordemo/src/test/java/com/perscholas/actuatordemo/ActuatordemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.actuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatordemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
