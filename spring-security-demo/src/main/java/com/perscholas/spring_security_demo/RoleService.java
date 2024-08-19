b8accd88dd84e4d603cef98836adcc57bb233732
sithuhantun91 <sithuhantun91@gmail.com>
2024-08-18 21:51:13 -0400

    commit GLAB - 309.8.1 - SP14 - Spring Security + Spring Data JPA + Thymeleaf with SecurityFilterChain

---
+++ spring-security-demo/src/main/java/com/perscholas/spring_security_demo/RoleService.java
@@ -0,0 +1,10 @@
+package com.perscholas.spring_security_demo;
+
+import java.util.List;
+
+public interface RoleService {
+    public void saveRole(Role role);
+    public Role findRoleByRoleName(String name);
+    public List<Role> getAllRoles();
+    public List<Role> getRolesByUser(long id);
+}
